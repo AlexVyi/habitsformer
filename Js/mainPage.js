edb50dc1f84f8de099f132fb2636e887d427f958
AlexVyi <alexandru@epk.ro>
2017-04-11 03:23:22 +0300

    v9-a lot has changed

--- Js/mainPage.js
+++ Js/mainPage.js
@@ -15,7 +15,7 @@ function changeTab(tabName){
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("mySidenav").style.width = "200px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
